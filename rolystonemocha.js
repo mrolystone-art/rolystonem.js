1ae1136737fadd66883d6fd571ea5dcfb4e77ad7
mrolystone-art <mrolystone@gmail.com>
2025-09-26 12:48:54 +0300

    Add files via upload

---
+++ rolystonemocha.js
@@ -0,0 +1,11 @@
+// function to check if a number is even
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+// test cases
+
+console.log(isEven(4));    // Expected output: true
+console.log(isEven(7));    // Expected output: false
+console.log(isEven(-2));   // Expected output: true
